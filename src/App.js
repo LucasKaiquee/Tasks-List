2f3a6ac524556f9634a812ab37bcf1985bae69d3
Lucas Kaique <lucaskaique743@gmail.com>
2022-10-11 19:53:06 -0300

    feat: fetch tasks from API; clean useless files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import {useState, useRef, useEffect} from 'react'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+
 import TaskItem from './components/TaskItem';
 
 const  App = () => {
-  const mounted = useRef(false);
-
-  useEffect(() => {
-      if(mounted.current === false){
-          mounted.current = true
-      } else console.log('component was updated!');
-  })
 
   const [tasks, setTasks] = useState([
     {
@@ -23,6 +18,21 @@ const  App = () => {
     }
   ]);
 
+  const fetchTasks = async () => {
+    try{
+      const {data} = await axios.get('https://fsc-task-manager-backend.herokuapp.com/tasks');
+      
+      setTasks(data);
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() =>{
+    fetchTasks()
+  }, [])
+
+
   return (
     <>
        {tasks.map((task) => (
@@ -33,52 +43,3 @@ const  App = () => {
 }
 
 export default App;
-/*
-import React from "react";
-import TaskItem from "./components/TaskItem";
-class App extends React.Component {
-  constructor (props) {
-    super(props)
-    this.handleStateChange = this.handleStateChange.bind(this);
-    this.state = {
-      tasks: [
-        {
-          id: 1,
-          description: 'Estudar programação',
-          isCompleted: false,
-        },
-        {
-          id: 2,
-          description: 'Estudar Enem',
-          isCompleted: true,
-        }
-      ]
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log(prevProps);
-    console.log(prevState);
-    console.log('component was updated!')
-  }
-
-  handleStateChange() {
-    this.setState({
-      tasks: [] ,
-    })
-  }
-
-  render() {
-    return (
-      <>
-         {this.state.tasks.map((task) => (
-            <TaskItem key={task.id} task={task}/>
-         ))}
-         <button onClick={this.handleStateChange}>Apagar</button>
-      </>
-    )
-  }
-}
-
-export default App;
-*/
--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,14 +1,5 @@
-import { useEffect, useState } from "react";
-
 const TaskItem = ({task}) => {
 
-    useEffect(() => {
-        console.log('component was mounted!');
-
-        return () => {
-            console.log('i will unmount!');
-        }
-    },[])
 
     return(
         <>
@@ -19,27 +10,5 @@ const TaskItem = ({task}) => {
 }
 
 export default TaskItem;
-//import React from "react";
 
 
-/*class TaskItem extends React.Component {
-    componentDidMount() {
-        console.log('component was mounted!');
-    }
-
-    componentWillUnmount() {
-        console.log('i will unmount!')
-    }
-    render() {
-        const {task} = this.props;
-
-        return(
-            <>
-                <h1>{task.description}</h1>
-                <p>{task.isCompleted ? 'Completa' : 'Incompleta'}</p>
-            </>
-        )
-    }
-}
-
-export default TaskItem;*/
