895b2e90d785366a0b45c2687577bd9a00478720
Lucas Kaique <lucaskaique743@gmail.com>
2023-01-12 15:45:52 -0300

    feat add alert in case of task addition success

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -31,6 +31,8 @@ const AddTask = ({fetchTasks}) => {
 
             await fetchTasks();
             setTask("");
+
+            await alert.success("A tarefa foi adicionada com seucesso.");
         } catch (error) {
             alert.error("Ops! Algo deu errado.")
         }
