e742dbd9284d0e7f684f3436d45a011889a09aff
Lucas Kaique <lucaskaique743@gmail.com>
2023-01-12 17:27:19 -0300

    perf: add last tasks and completed tasks to useMemo

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import axios from "axios";
 import { useAlert } from "react-alert";
 
@@ -15,12 +15,20 @@ const Tasks = () =>{
       const fetchTasks = async () => {
         try{
           const {data} = await axios.get('https://fsc-taskmanager-api.up.railway.app/tasks');
-          
+
           setTasks(data);
         } catch(_error) {
             alert.error('Não foi possível recuperar as tarefas.');
         }
       }
+
+      const lastTasks = useMemo(() =>{
+          return tasks.filter(task =>  task.isCompleted === false);
+      }, [tasks])
+
+      const completedTasks = useMemo(() => {
+          return tasks.filter(task => task.isCompleted === true);
+      }, [tasks]) 
     
       useEffect(() =>{
         fetchTasks()
@@ -35,8 +43,7 @@ const Tasks = () =>{
             <h3>Últimas Tarefas </h3>
                 <AddTask fetchTasks={fetchTasks} />
                 <div className="tasks-list">
-                    {tasks
-                        .filter((task) => task.isCompleted === false)
+                    {lastTasks
                         .map((lastTask) => (
                             <TaskItem key={lastTask._id} task ={lastTask} fetchTasks={fetchTasks}/>
                         ))}
@@ -46,7 +53,7 @@ const Tasks = () =>{
             <div className="completed-tasks">
                 <h3>Tarefas Concluídas</h3>
                 <div className="tasks-list">
-                    {tasks.filter(task => task.isCompleted).map(completedTask =>(
+                    {completedTasks.map(completedTask =>(
                         <TaskItem key={completedTask._id} task ={completedTask} fetchTasks={fetchTasks}/>
                     ))}
                 </div>
