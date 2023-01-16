e218fc2e53291bfa9332db92cce391297c68d94d
Lucas Kaique <lucaskaique743@gmail.com>
2023-01-16 11:37:03 -0300

    fix: login styles imports

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,12 @@
 import CustomButton from "../components/CustomButton"
 
-import "./Logo.scss"
+import "./Login.scss"
 import logo from "../assets/images/logo.png"
 
 const Login = () => {
     return(
         <div className="login-container">
-            <img src={logo=""} alt="React Pro" />
+            <img src={logo} alt="React Pro" />
             <div className="button-container">
                 <CustomButton>Entrar</CustomButton>
             </div>
