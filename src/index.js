595b5c317e258d566c4e7620f95e878235a65fff
Lucas Kaique <lucaskaique743@gmail.com>
2023-01-21 13:55:38 -0300

    fix: correction eslint from js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { positions, transitions, Provider as AlertProvider } from 'react-alert';
-import AlertTemplate from 'react-alert-template-basic';
-import {BrowserRouter} from "react-router-dom"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { positions, transitions, Provider as AlertProvider } from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
+import { BrowserRouter } from 'react-router-dom'
 
-import './index.scss';
-import reportWebVitals from './reportWebVitals';
+import './index.scss'
+import reportWebVitals from './reportWebVitals'
 
-import App from './App';
+import App from './App'
 
 const options = {
-    position: positions.BUTTON_CENTER,
-    timeout: 2500,
-    offset: '30px',
-    trasition: transitions.SCALE
+  position: positions.BUTTON_CENTER,
+  timeout: 2500,
+  offset: '30px',
+  trasition: transitions.SCALE
 }
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <BrowserRouter>
@@ -26,9 +25,9 @@ root.render(
       </AlertProvider>
     </BrowserRouter>
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
